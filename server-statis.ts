94ce196b94d22f6f67585b42e80d53c7b3affbd3
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-05-13 19:53:59 +0700

    feat: add pipeline and read stream

--- server-statis.ts
+++ server-statis.ts
@@ -1,12 +1,13 @@
 import http, { IncomingMessage, ServerResponse } from "node:http";
 import path from "node:path";
-import fs from "node:fs/promises";
+import fs from "node:fs";
+import { pipeline } from "node:stream/promises";
+
 
 interface FileMetaData {
-  status : number
-  filename : string
+  status: number;
+  filename: string;
 }
-
 const mimeMap: Record<string, string> = {
   ".html": "text/html",
   ".png": "image/png",
@@ -14,34 +15,37 @@ const mimeMap: Record<string, string> = {
   ".js": "application/javascript",
 };
 const urlMap: Record<string, FileMetaData> = {
-  "/": {status : 200, filename : "/index.html"},
-  "/image": {status : 200, filename : "/image.png"},
-  "/style": {status : 200, filename : "/style.css"},
-  "/script": {status : 200, filename : "/script.js"},
+  "/": { status: 200, filename: "/index.html" },
+  "/image": { status: 200, filename: "/image.png" },
+  "/index" : {status : 200, filename : "/index.html"},
+  "/style": { status: 200, filename: "/style.css" },
+  "/script": { status: 200, filename: "/script.js" },
 };
+
+const PUBLIC_DIR = path.join(import.meta.dirname, "public");
 
 console.log("Script dimulai...");
 const server = http.createServer(
   async (req: IncomingMessage, res: ServerResponse) => {
-
     try {
-      const targetUrl = urlMap[req.url!]
-
+      const targetUrl = urlMap[req.url!];
       if (!targetUrl) throw new Error("url tidak valid");
-      const data = await fs.readFile(
-        path.join(import.meta.dirname, "public", targetUrl.filename),
-      );
 
-      if (!data) throw new Error("FIle tidak ditemukan");
-      
-      const ext = path.extname(targetUrl.filename)
-      res.writeHead(200, { "content-type": mimeMap[ext] });
-      res.end(data);
+      const targetFile = path.join(PUBLIC_DIR, targetUrl.filename);
+      const ext = path.extname(targetUrl.filename);
+
+      res.writeHead(200, { "content-type": mimeMap[ext] || "text/plain" });
+
+      await pipeline(fs.createReadStream(targetFile), res);
+      console.log("Stream berhasil");
+
     } catch (err) {
-      const error = err as Error
-      console.error(error.message);
-      res.writeHead(404, { "content-type": "text/plain" });
-      res.end("Page is not found");
+      const error = err as Error;
+      console.error(
+        `Gagal memuat URL: "${req.url}" -> Alasan: ${error.message}`,
+      );
+      res.writeHead(404);
+      res.end("Page not found \nPlease acces /image, /script, or /style");
     }
   },
 );
