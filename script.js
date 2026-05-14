bd8d738cf0c7637d1981ba90dc8612a505ef5dbb
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-05-14 21:08:01 +0700

    init image-parser.ts

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs").promises; 
+import fs from "node:fs"
+import path from "node:path";
 
-const mapMime = {
-  ".html": "text/html",
-  ".png": "image/png",
-  ".css": "text/css",
-  ".js": "application/javascript",
-};
-
-const server = http.createServer(
-  async (req, res) => {
-    if (req.url === "/") {
-      try {
-        const data = await fs.readFile(
-          path.join(__dirname, "public", "image.png"),
-        );
-        res.writeHead(200, { "content-type": "image/png" });
-        res.end(data);
-      } catch (err) {
-        res.writeHead(404, { "content-type": "text/plain" });
-        res.end("Page is not found");
-      }
-    } else {
-      res.writeHead(404, { "content-type": "text/plain" });
-      res.end("Page is not found");
-    }
-  },
-);
-
-server.listen(3000, () => {
-  console.log("Server siap! Buka http://localhost:3000 di browsermu.");
-});
+const image = path.join(import.meta.dirname, "public", "image.png")
+const buf = fs.readFileSync(image)
+console.log(buf.length);
