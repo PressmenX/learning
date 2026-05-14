73628f9f2c35b58d1a606a742e5baf65369c8edf
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-05-14 22:17:29 +0700

    feat: complete image-parser.ts

---
+++ ts/image-parser.ts
@@ -0,0 +1,39 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const FILE_NAME = process.argv[2] ?? "nodejs.png";
+const IMAGE_PATH = path.join(import.meta.dirname, "..", "public", FILE_NAME);
+
+function imageParser(filepath: string): void {
+  let fd: number | null = null;
+  try {
+    fd = fs.openSync(filepath, "r");
+
+    const stat = fs.fstatSync(fd)
+    const buffer = Buffer.alloc(24);
+    fs.readSync(fd, buffer, 0, 24, 0);
+
+    const notPng = buffer.toString("utf-8", 1, 4) !== "PNG";
+    if (notPng) throw new Error("file extension is not png");
+
+    console.log("--------------------------------");
+    console.log(
+      `File name : ${FILE_NAME}\nSize : ${Math.floor(stat.size/ 1024)} KB\nWidth : ${buffer.readInt32BE(16)} px\nHeight : ${buffer.readInt32BE(20)} px`,
+    );
+    console.log("--------------------------------");
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      const nodeErr = err as NodeJS.ErrnoException;
+      if (nodeErr.code === "ENOENT") console.log("File not found");
+      else console.log(`Error : ${err.message}`);
+    } else {
+      console.log(`Another Error : ${err}`);
+    }
+  } finally {
+    if (fd !== null) {
+      fs.closeSync(fd);
+    }
+  }
+}
+
+imageParser(IMAGE_PATH);
