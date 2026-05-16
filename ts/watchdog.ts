d797fb09a8b042787718a8d255dd6d2a8eefbabd
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-05-16 16:00:09 +0700

    feat: add max restart handling to  watchdog.ts

--- ts/watchdog.ts
+++ ts/watchdog.ts
@@ -1,13 +1,23 @@
 import { spawn } from "node:child_process";
 
+const maxRestart = 5
+let currRestart = 0 
 function monitorService(): void {
   const child = spawn("ts-node", ["ts/app.ts"], { stdio: "inherit" });
 
   child.on("exit", (code) => {
+    currRestart++
     console.log(
-      `[WATCHDOG] Service is exit with code ${code}, Tried restarting the service...`,
+      `[WATCHDOG] Service is exit with code ${code}, Tried restarting the service... (×${currRestart})`,
     );
-    setTimeout(() => monitorService(), 1000);
+
+    if (currRestart >= maxRestart) {
+      console.log("Automated restart limit reached. Action required: Please run the restart command");
+      return
+    }
+    setTimeout(() => {
+      monitorService()
+    }, 1000);
   });
 }
 
