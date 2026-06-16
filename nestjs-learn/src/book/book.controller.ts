7906eee533a8a9cc854c1a461f65084b4aaad43f
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-06-16 23:01:52 +0700

    feat: update book API to use correct authorId and implement API key guard

--- nestjs-learn/src/book/book.controller.ts
+++ nestjs-learn/src/book/book.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateBookUseCase } from './use-cases/create-book.use-case';
 import { CreateBookDTO } from './dto/createBook.dto';
+import { ApiKeyGuard } from 'src/guard/api-key/api-key.guard';
 
 @Controller('books')
+@UseGuards(ApiKeyGuard)
 export class BookController {
   constructor(private readonly createBook: CreateBookUseCase) {}
   @Post()
