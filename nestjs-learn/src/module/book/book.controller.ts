83e5c63d28c5a494dcee9a61af70a3858f97499c
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-06-17 22:48:14 +0700

    feat: enhance book creation API with detailed Swagger documentation and fix API key in HTTP request

--- nestjs-learn/src/module/book/book.controller.ts
+++ nestjs-learn/src/module/book/book.controller.ts
@@ -2,12 +2,35 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateBookUseCase } from './use-cases/create-book.use-case';
 import { CreateBookDTO } from './dto/createBook.dto';
 import { ApiKeyGuard } from 'src/guard/api-key/api-key.guard';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Books')
+@ApiSecurity('api-key')
 @Controller('books')
 @UseGuards(ApiKeyGuard)
 export class BookController {
   constructor(private readonly createBook: CreateBookUseCase) {}
+
   @Post()
+  @ApiOperation({ summary: 'Create a new book' })
+  @ApiResponse({
+    status: 201,
+    description: 'The book has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request or Invalid input data.',
+  })
+  @ApiResponse({
+    status: 403,
+    description:
+      "API Key not found. Please include 'x-api-key' in the request header.",
+  })
   create(@Body() payload: CreateBookDTO) {
     return this.createBook.execute(payload);
   }
--- nestjs-learn/src/module/book/dto/createBook.dto.ts
+++ nestjs-learn/src/module/book/dto/createBook.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateBookDTO {
+  @ApiProperty({ example: 'b-1' })
   @IsString({ message: 'invalid id format, The id must be a string' })
   @IsNotEmpty()
   id!: string;
 
+  @ApiProperty({ example: 'The Home', minLength: 3 })
   @IsString({ message: 'The book name must be a string' })
   @IsNotEmpty()
   @MinLength(3, { message: 'Book name must be at least 3 characters' })
   name!: string;
 
+  @ApiProperty({ example: 'a-2' })
   @IsString({
     message: 'invalid authorId format, The authorId must be a string',
   })
