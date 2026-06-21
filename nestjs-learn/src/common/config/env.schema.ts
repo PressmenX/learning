2ebb736c4a17cf477a91aaf5659324af32ea4989
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-06-21 16:11:17 +0700

    feat: add contact limit validation in create contact use case

--- nestjs-learn/src/common/config/env.schema.ts
+++ nestjs-learn/src/common/config/env.schema.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
     .default('development'),
   PORT: z.coerce.number().default(3000),
   API_KEY: z.string().min(1),
+  MAX_CONTACT_PER_USER: z.coerce.number(),
 });
 
 export default envSchema;
--- nestjs-learn/src/modules/contacts/use-cases/create-contact.use-case.ts
+++ nestjs-learn/src/modules/contacts/use-cases/create-contact.use-case.ts
@@ -2,12 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { ContactsRepositoryAbstract } from '../interfaces/contacts.repository.abstract';
 import { CreateContactDto } from '../DTOs/create-contact.dto';
 import { nanoid } from 'nanoid';
+import { ConfigService } from '@nestjs/config';
+import { ContactLimitReachedError } from '../errors/contact-limit-reached.error';
 
 @Injectable()
 export class CreateContactUseCase {
-  constructor(private readonly contactsRepo: ContactsRepositoryAbstract) {}
+  constructor(
+    private readonly contactsRepo: ContactsRepositoryAbstract,
+    private readonly configService: ConfigService,
+  ) {}
 
   execute(payload: CreateContactDto) {
+    const maxContact = Number(
+      this.configService.get<number>('MAX_CONTACT_PER_USER'),
+    );
+    const totalCurrentContacts = this.contactsRepo.findAll().length;
+
+    if (totalCurrentContacts >= maxContact) {
+      throw new ContactLimitReachedError(
+        'The available contact capacity has reached its limit',
+      );
+    }
+
     const newItem = {
       id: 'con-' + nanoid(),
       ...payload,
