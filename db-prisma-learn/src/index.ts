a0e1292b40eac03b8c2b9907480a45ebc7215d0b
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-06-22 22:44:38 +0700

    Refactor code structure for improved readability and maintainability

--- db-prisma-learn/src/index.ts
+++ db-prisma-learn/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client";
 import "dotenv/config";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
@@ -11,9 +11,26 @@ const prisma = new PrismaClient({
 const main = async () => {
   console.log("Prisma connection successfully connected");
 
-  const allUsers = await prisma.user.findMany();
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: "Joko",
+  //     email : "Joko12@gmail.com",
+  //   },
+  // });
 
+  const allUsers = await prisma.user.findMany();
   console.log(allUsers);
+
+  // const userUpdated = await prisma.user.update({
+  //   where: { id: 2 },
+  //   data: { name: "Joko Widodo", email: "joko777@gmail.com" },
+  // });
+  // console.log(`User Data ${userUpdated.name} after update : `, userUpdated);
+
+  const userDeleted = await prisma.user.delete({
+    where: { id: 1 },
+  });
+  console.log(`User with ${userDeleted.id} deleted successful :`, userDeleted);
 };
 
 main()
