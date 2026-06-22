83753e4c3c8d15ba98a5bc6f704929dcae9c0f8e
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-06-22 19:39:40 +0700

    feat: add Prisma setup with PostgreSQL adapter, update schema, and configure TypeScript

---
+++ db-prisma-learn/src/index.ts
@@ -0,0 +1,24 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({
+  adapter,
+  log: ["query", "info", "warn", "error"],
+});
+
+const main = async () => {
+  console.log("Prisma connection successfully connected");
+
+  const allUsers = await prisma.user.findMany();
+
+  console.log(allUsers);
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
